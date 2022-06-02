3f3ec9234e5dff0b70b612b88acffa2b511d5d35
Abu Anwar <abuanwar072@gmail.com>
2022-06-02 16:27:06 +0600

    update path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/constants.dart';
-import 'package:flutter_auth/screens/welcome_screen.dart';
+import 'package:flutter_auth/screens/welcome/welcome_screen.dart';
 
 void main() => runApp(const MyApp());
 
